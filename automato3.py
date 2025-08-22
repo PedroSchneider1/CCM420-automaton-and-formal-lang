37fec759ed02424fb48643780f337f36c8242245
Pedro Schneider <162492569+PedroSchneider1@users.noreply.github.com>
2025-08-21 22:24:21 -0300

    Salvo em 21-08-2025 22:24:21

---
+++ automato3.py
@@ -0,0 +1,9 @@
+# Construir AFDs que reconheçam as seguintes linguagens e represente-os formalmente:
+"""
+L1 = {w ∈ {0, 1}∗: w termina em 01}
+L2 = {w ∈ {a, b}∗: w começa e termina com o mesmo símbolo}
+L3 = {w ∈ {0, 1}∗: toda posição ímpar de w é 1}
+L4 = {w ∈ {0, 1}∗: w não contém 110 como subcadeia}
+L5 = {w ∈ {0, 1}∗: todo 0 em w é seguido de pelo menos um 1}
+L6 = {w ∈ {0, 1}∗: w contém ao menos dois 0s e no máximo um 1}
+"""
